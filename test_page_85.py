a980d8a5ad35075c2266e768e049dfdba0f0cc16
Alex4Game <s2dent_84@mail.ru>
2024-06-07 23:34:33 +0300

    96_1

---
+++ test_page_85.py
@@ -0,0 +1,14 @@
+simple_array = [[2, 5, 8], [3, 7, 4], [1, 6, 9]]
+print(simple_array)
+simple_array [2] [1] = 5
+print(simple_array)
+print(simple_array[1])
+simple_array[1].append(33)
+print(simple_array)
+data_set = {"A":{"x":54, "y": 82, "z": 91}, "B":{"x":75, "y": 29, "z": 80}}
+print(data_set)
+print(data_set["A"])
+for i in data_set:
+    print(data_set [i] ["y"])
+data_set ["B"] ["y"] = 555
+print(data_set)
